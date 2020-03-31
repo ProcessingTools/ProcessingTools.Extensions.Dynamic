f09515b433086916740b2a7926035cf4a6c32d62
Bozhin Karaivanov <bozhin.karaivanov@live.com>
2020-03-31 10:28:31 +0300

    Update MapperConfigurationExtensions

--- ProcessingTools.Extensions.Dynamic/ProcessingTools.Extensions.Dynamic.AutoMapper/MapperConfigurationExtensions.cs
+++ ProcessingTools.Extensions.Dynamic/ProcessingTools.Extensions.Dynamic.AutoMapper/MapperConfigurationExtensions.cs
@@ -13,49 +13,46 @@ namespace ProcessingTools.Extensions.Dynamic.AutoMapper
     public static class MapperConfigurationExtensions
     {
         /// <summary>
-        /// Creates a mapping configuration from the TSource type to the TDestination type with proxy type.
+        /// Creates a mapping configuration from the TSource type to the TDestination type using proxy type.
         /// </summary>
         /// <typeparam name="TSource">Source type.</typeparam>
         /// <typeparam name="TDestination">Destination type.</typeparam>
         /// <param name="configuration">Instance of <see cref="IMapperConfigurationExpression"/> to be updated.</param>
-        /// <returns>Mapping expression for more configuration options.</returns>
-        public static IMappingExpression<TSource, TDestination> CreateMapWithProxy<TSource, TDestination>(this IMapperConfigurationExpression configuration)
+        public static void CreateMapUsingProxy<TSource, TDestination>(this IMapperConfigurationExpression configuration)
         {
             if (configuration is null)
             {
                 throw new ArgumentNullException(nameof(configuration));
             }
 
-            return (IMappingExpression<TSource, TDestination>)CreateMapWithProxy(configuration, sourceType: typeof(TSource), destinationType: typeof(TDestination));
+            CreateMapUsingProxy(configuration, sourceType: typeof(TSource), destinationType: typeof(TDestination));
         }
 
         /// <summary>
-        /// Creates a mapping configuration from the TSource type to the TDestination type with proxy type.
+        /// Creates a mapping configuration from the TSource type to the TDestination type using proxy type.
         /// Specify the member list to validate against during configuration validation.
         /// </summary>
         /// <typeparam name="TSource">Source type.</typeparam>
         /// <typeparam name="TDestination">Destination type.</typeparam>
         /// <param name="configuration">Instance of <see cref="IMapperConfigurationExpression"/> to be updated.</param>
         /// <param name="memberList">Member list to validate.</param>
-        /// <returns>Mapping expression for more configuration options.</returns>
-        public static IMappingExpression<TSource, TDestination> CreateMapWithProxy<TSource, TDestination>(this IMapperConfigurationExpression configuration, MemberList memberList)
+        public static void CreateMapUsingProxy<TSource, TDestination>(this IMapperConfigurationExpression configuration, MemberList memberList)
         {
             if (configuration is null)
             {
                 throw new ArgumentNullException(nameof(configuration));
             }
 
-            return (IMappingExpression<TSource, TDestination>)CreateMapWithProxy(configuration, sourceType: typeof(TSource), destinationType: typeof(TDestination), memberList: memberList);
+            CreateMapUsingProxy(configuration, sourceType: typeof(TSource), destinationType: typeof(TDestination), memberList: memberList);
         }
 
         /// <summary>
-        /// Creates a mapping configuration from the source type to the destination type with proxy type if destinationType is an interface.
+        /// Creates a mapping configuration from the source type to the destination type using proxy type if destinationType is an interface.
         /// </summary>
         /// <param name="configuration">Instance of <see cref="IMapperConfigurationExpression"/> to be updated.</param>
         /// <param name="sourceType">Source type.</param>
         /// <param name="destinationType">Destination type.</param>
-        /// <returns>Mapping expression for more configuration options.</returns>
-        public static IMappingExpression CreateMapWithProxy(this IMapperConfigurationExpression configuration, Type sourceType, Type destinationType)
+        public static void CreateMapUsingProxy(this IMapperConfigurationExpression configuration, Type sourceType, Type destinationType)
         {
             if (configuration is null)
             {
@@ -75,24 +72,25 @@ namespace ProcessingTools.Extensions.Dynamic.AutoMapper
             if (destinationType.IsInterface)
             {
                 Type destinationTypeProxy = DynamicProxyBuilder.ModuleBuilder.GetProxyTypeOf(destinationType);
-                return configuration.CreateMap(sourceType: sourceType, destinationType: destinationTypeProxy);
+                configuration.CreateMap(sourceType: sourceType, destinationType: destinationTypeProxy);
+                configuration.CreateMap(sourceType: destinationType, destinationType: destinationTypeProxy);
+                configuration.CreateMap(sourceType: sourceType, destinationType: destinationType).As(destinationTypeProxy);
             }
             else
             {
-                return configuration.CreateMap(sourceType: sourceType, destinationType: destinationType);
+                configuration.CreateMap(sourceType: sourceType, destinationType: destinationType);
             }
         }
 
         /// <summary>
-        /// Creates a mapping configuration from the source type to the destination type with proxy type if destinationType is an interface.
+        /// Creates a mapping configuration from the source type to the destination type using proxy type if destinationType is an interface.
         /// Specify the member list to validate against during configuration validation.
         /// </summary>
         /// <param name="configuration">Instance of <see cref="IMapperConfigurationExpression"/> to be updated.</param>
         /// <param name="sourceType">Source type.</param>
         /// <param name="destinationType">Destination type.</param>
         /// <param name="memberList">Member list to validate.</param>
-        /// <returns>Mapping expression for more configuration options.</returns>
-        public static IMappingExpression CreateMapWithProxy(this IMapperConfigurationExpression configuration, Type sourceType, Type destinationType, MemberList memberList)
+        public static void CreateMapUsingProxy(this IMapperConfigurationExpression configuration, Type sourceType, Type destinationType, MemberList memberList)
         {
             if (configuration is null)
             {
@@ -112,11 +110,13 @@ namespace ProcessingTools.Extensions.Dynamic.AutoMapper
             if (destinationType.IsInterface)
             {
                 Type destinationTypeProxy = DynamicProxyBuilder.ModuleBuilder.GetProxyTypeOf(destinationType);
-                return configuration.CreateMap(sourceType: sourceType, destinationType: destinationTypeProxy, memberList: memberList);
+                configuration.CreateMap(sourceType: sourceType, destinationType: destinationTypeProxy, memberList: memberList);
+                configuration.CreateMap(sourceType: destinationType, destinationType: destinationTypeProxy, memberList: memberList);
+                configuration.CreateMap(sourceType: sourceType, destinationType: destinationType, memberList: memberList).As(destinationTypeProxy);
             }
             else
             {
-                return configuration.CreateMap(sourceType: sourceType, destinationType: destinationType, memberList: memberList);
+                configuration.CreateMap(sourceType: sourceType, destinationType: destinationType, memberList: memberList);
             }
         }
     }
